091e5fa04a2637522610926d39447a9f5981647a
Sushsnat <grtsushant@gmail.com>
2023-12-30 11:52:18 +0530

    modified cors allow all origions

--- index.js
+++ index.js
@@ -15,17 +15,8 @@ const app = express();
   app.use(express.json());
 
 
-  const allowedOrigins = ['http://localhost:3000'];
 
-    app.use(cors({
-      origin: function (origin, callback) {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-          callback(null, true);
-        } else {
-          callback(new Error('Not allowed by CORS'));
-        }
-      },
-    }));
+    app.use(cors());
 
 app.use("/api/user", userrouter);
 app.use("/api/chat", chatrouter);
